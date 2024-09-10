d2090b6180820775a96040e4cf777e5f7f63011d
Edyane <edyanealves@gmail.com>
2024-09-09 21:24:46 -0300

    feat: adding second project

---
+++ users-project/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import UserForm from './components/userForm/UserForm.js';
+import UserList from './components/userList/UserList.js';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
+  return (
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
+    </div>
+  );
+}
+
+export default App;
