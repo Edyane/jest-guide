fe43e1d9f26335d6a0ebb3b3f0c1798faef83c3a
Edyane <edyanealves@gmail.com>
2025-02-06 10:49:11 -0300

    test: render the correct number of rows, email and name of each user

--- users-project/src/components/userList/UserList.test.js
+++ users-project/src/components/userList/UserList.test.js
@@ -1,19 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import UserList from './UserList';
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
 
-test('render the correct number of rows', () => {
-    //Render the component
-    const users = [
-        { name: 'jane', email: 'jane@jane.com' },
-        { name: 'sam', email: 'sam@sam.com' },
-    ];
-    render(<UserList users={users} />);
+const renderComponent = () => {
+	const users = [
+		{ name: "jane", email: "jane@jane.com" },
+		{ name: "sam", email: "sam@sam.com" },
+	];
+	render(<UserList users={users} />);
 
-    //Find all the rows in the table
+	return {
+		users,
+	}
+};
 
-    //Assertion: correct number of rows in the table
+test("render the correct number of rows", () => {
+	//Render the component
+	renderComponent();
+
+	//Find all the rows in the table
+	const rows = within(screen.getByTestId("users")).getAllByRole("row");
+
+	//Assertion: correct number of rows in the table
+	expect(rows).toHaveLength(2);
 });
 
-test('render the email and name of each user', () => {
+test("render the email and name of each user", () => {
+	const { users } = renderComponent();
 
+	for (let user of users) {
+		const name = screen.getByRole("cell", { name: user.name });
+		const email = screen.getByRole("cell", { name: user.email });
+
+		expect(name).toBeInTheDocument();
+		expect(email).toBeInTheDocument();
+	}
 });
